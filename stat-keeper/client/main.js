5951f5af4c2f752d894ff88629a81c9339695d7f
msedlazek <mark.sedlazek@gmail.com>
2017-04-29 09:54:45 -0400

    Copied over working meteor files.

--- stat-keeper/client/main.js
+++ stat-keeper/client/main.js
@@ -1,22 +1,47 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
 
-import './main.html';
+import {Players} from './../imports/api/players';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const renderPlayers = function (playersList) {
+	return playersList.map(function (player) {
+		return <p key={player._id}>{player.name} has {player.score} point(s)!</p>;
+	});
+};
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+const handleSubmit = function (e) {
+	let playerName = e.target.playerName.value;
+	e.preventDefault();
+	if (playerName) {
+		e.target.playerName.value = '';
+		Players.insert({
+			name: playerName,
+			score: 0
+		});
+	};
+};
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+/*Meteor.startup will run when the DOM is ready here, Tracker will execute whenever something changes in the database.*/
+Meteor.startup(function () {
+	Tracker.autorun(function () {
+		let players = Players.find().fetch();
+		let title = "Score Keep";
+		let name = 'Mark Sedlazek';
+		let jsx = (
+			<div>
+				<h1>{title}</h1>
+				<p>Hello {name}!</p>
+				<p>Here are the players we are tracking.</p>
+				{renderPlayers(players)}
+				<form onSubmit={handleSubmit}>
+					<input type="text" name="playerName" placeholder="Player Name"/>
+					<button>Add Player</button>
+				</form>
+			</div>
+		);
+		ReactDOM.render(jsx, document.getElementById('app'));	
+	});
+	console.log(Players.find().fetch());
 });
--- stat-keeper/server/main.js
+++ stat-keeper/server/main.js
@@ -1,5 +1,35 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(function () {
+	/*Players.insert({
+		name: 'Tanis',
+		score: 10
+	});
+	console.log(Players.find().fetch());*/
+	
+	/*let square = function (x) {
+		return x * x;
+	};*/
+
+	/* this is an arrow function, works like a normal function 
+	let square = (x) => x * x;
+	
+	
+	console.log(square(10));
+	*/
+
+	let user = {
+		name: 'Mark',
+		sayHi () {
+			setTimeout(() => {
+				console.log(this.name);
+			}, 1000);
+		}
+	};
+	user.sayHi(1, 2);
+
+	let numbers = [9,99,4,56];
+	let newNumbers = numbers.map((number) => number + 1);
+	console.log(newNumbers);
 });
